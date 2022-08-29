480fcc49f074bf201a7772af06106e715c51bc4e
mahmoud fouad <mfapolice@gmail.com>
2022-08-29 04:51:44 +0200

    first commet

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+import './ExpenseItem.css';
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>August 24th 1991</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
