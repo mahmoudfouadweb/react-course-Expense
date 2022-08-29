bc82cb14b6645f071f86b5f08bb2b93be27008c4
mahmoud fouad <mfapolice@gmail.com>
2022-08-30 01:59:58 +0200

    update

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,12 @@
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+
+const NewExpense = () => {
+  return (
+    <div>
+      <ExpenseForm />
+    </div>
+  );
+};
+
+export default NewExpense;
