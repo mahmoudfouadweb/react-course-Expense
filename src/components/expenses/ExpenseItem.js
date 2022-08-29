4d6f2978b055ee3eaf4b9a6989e91d8828a2e097
mahmoud fouad <mfapolice@gmail.com>
2022-08-30 01:42:55 +0200

    update

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -3,19 +3,19 @@ import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 function ExpenseItem(props) {
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState(props.title);
+
   function clickHandler() {
-    if (title === 'Updated') {
-      setTitle(props.title);
-    } else {
-      setTitle('Updated');
-    }
+    setTitle('Updated');
+    setTimeout(() => setTitle(props.title), 1500);
   }
+  console.log(title);
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title ? title : props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
       <button onClick={clickHandler}>change title</button>
