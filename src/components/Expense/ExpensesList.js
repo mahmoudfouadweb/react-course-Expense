a164b0f933cd14ec739413fbb37d2f0d8f0ff718
mahmoud fouad <mfapolice@gmail.com>
2022-09-01 04:51:33 +0200

    logic done

--- src/components/Expense/ExpensesList.js
+++ src/components/Expense/ExpensesList.js
@@ -9,22 +9,22 @@ function ExpensesList(props) {
     setSelectedYear(theYear);
     props.onFilter(theYear);
   };
-
-  console.log('selectedYear', selectedYear);
+  let expenesesContent = <h1>No Data Found...</h1>;
+  if (props.expenses.length > 0) {
+    expenesesContent = props.expenses.map((expense, i) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
   return (
     <div>
       <div className="expenses">
         <ExpensesFilter onYear={selectedYear} onAddYear={addEnteredYear} />
-        {props.expenses.length === 0 && <h1>No Data Found... </h1>}
-        {props.expenses.length > 0 &&
-          props.expenses.map((expense, i) => (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          ))}
+        {expenesesContent}
       </div>
     </div>
   );
